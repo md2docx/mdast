330464a1e57470412e7d4fb17ad13534835a34b4
Mayank <mayank.srmu@gmail.com>
2025-05-19 14:36:45 +0530

    docs(changeset): Fix type utilities to handle function types

---
+++ .changeset/spotty-pumas-sip.md
@@ -0,0 +1,5 @@
+---
+"@m2d/mdast": patch
+---
+
+Fix type utilities to handle function types
--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -1,5 +1,19 @@
-export type Optional<T> = { [K in keyof T]?: T[K] extends object ? Optional<T[K]> : T[K] };
+export type Optional<T> = {
+  // skipcq: JS-0296
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  [K in keyof T]?: T[K] extends object ? (T[K] extends Function ? T[K] : Optional<T[K]>) : T[K];
+};
 
-export type Required<T> = { [K in keyof T]-?: T[K] extends object ? Required<T[K]> : T[K] };
+export type Required<T> = {
+  // skipcq: JS-0296
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  [K in keyof T]-?: T[K] extends object ? (T[K] extends Function ? T[K] : Required<T[K]>) : T[K];
+};
 
-export type Mutable<T> = { -readonly [K in keyof T]: T[K] extends object ? Mutable<T[K]> : T[K] };
+export type Mutable<T> = {
+  -readonly [K in keyof T]: T[K] extends object
+    ? T[K] extends Function
+      ? T[K]
+      : Mutable<T[K]>
+    : T[K];
+};
